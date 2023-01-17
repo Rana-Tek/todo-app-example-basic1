c594e4231363cb39e178a6ac6daef35dc0d12100
Rana-Tek <ranatekbas122@gmail.com>
2023-01-17 17:53:59 +0300

    first coomit: create html and css folder, js folder is cretaing

---
+++ app.js
@@ -0,0 +1,10 @@
+window.onload = loadTasks;
+function loadTasks () {
+    let tasks = localStorage.getItem("tasks")
+
+    tasks.forEach(task => {
+        const list = document.querySelector("ul")
+        const li = document.querySelector(li)
+        li.innerHTML = 
+    })
+}
